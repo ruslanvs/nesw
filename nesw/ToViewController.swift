276d0b7f17c9fb6da27242bf41f1dd9a8bda3bec
ruslanvs <ruslanvs@yahoo.com>
2018-03-13 19:52:18 -0700

    Blank views

---
+++ nesw/ToViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ToViewController.swift
+//  nesw
+//
+//  Created by Ruslan Suvorov on 3/13/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class ToViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
